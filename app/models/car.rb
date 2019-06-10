f0d5e7d31738923ee8ede05508ad9c5fe63f391e
Anne Marie O'Sullivan <annemarieos.irl@gmail.com>
2019-06-10 21:54:54 +0100

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,20 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :mechanic, :car_owner, :classification
+  @@all = []
 
-  def initialize(make, model, classification)
+  def initialize(make, model, mechanic, car_owner, classification)
     @make = make
     @model = model
+    @mechanic = mechanic
+    @car_owner = car_owner
     @classification = classification
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,34 @@
 class Mechanic
 
-  attr_reader :name, :specialty
+  attr_reader :name, :specialty, :car
+  @@all = []
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select {|car| car.mechanic == self}
+  end
+  #.select runs expression for an array
+
+  def car_owners
+     cars.map {|car| car.car_owner}
+  end
+  #.map performs an action on each element within an array
+
+  def car_owner_names
+    car_owner.map {|car_owner| car_owner.name}
+  end
+
+
+
 
 
 end
